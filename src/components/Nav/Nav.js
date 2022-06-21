74b7472ad6d95fc7095e31baa49fec6c52f70aab
hjpark625 <zero950@gmail.com>
2022-06-21 20:04:57 +0900

    ADD: 초기 레이아웃 일부 완성

--- src/components/Nav/Nav.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './nav.scss';
-
-const Nav = () => {
-  return <div>컴포넌트</div>;
-};
-
-export default Nav;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import './Main.scss';
 
 const Main = () => {
-  return <div>메인</div>;
+  return (
+    <>
+      {/* nav제작 후 붙이기 */}
+      <div
+        className="navBar"
+        style={{
+          backgroundColor: 'yellow',
+          height: '100px',
+          position: 'fixed',
+          width: '100%',
+          top: '0',
+        }}
+      >
+        nav
+      </div>
+      <div className="main">
+        <section className="slideSection">슬라이드 섹션</section>
+        <section className="productSection">
+          <div className="shelfFilter">
+            <div className="buttonContainer">
+              <div className="categoryButton">
+                <button>
+                  <span className="buttonText">카테고리</span>
+                </button>
+              </div>
+              <div className="alignButton">
+                <button>
+                  <span>정렬 선택 버튼</span>
+                </button>
+              </div>
+            </div>
+            <p>메이크업 24개 상품</p>
+          </div>
+          <div className="productShelf">
+            <Products />
+          </div>
+        </section>
+      </div>
+      {/* Footer 제작 후 붙이기 */}
+      <div style={{ backgroundColor: 'green', height: '200px' }}>Footer</div>
+    </>
+  );
+};
+
+const Products = () => {
+  const [productData, setProductData] = useState([]);
+  useEffect(() => {
+    fetch('data/productData.json')
+      .then(res => res.json())
+      .then(data => {
+        setProductData(data);
+      });
+  });
+  return (
+    <>
+      {productData.map(({ id, img, title, hashtxt1, hashtxt2 }) => {
+        return (
+          <div className="productContainer" key={id}>
+            <Link to="#" className="productItemLink">
+              <div className="productItemThumb">
+                <img src={img} alt="thumb1" />
+              </div>
+            </Link>
+            <div className="productItemHashWrap">
+              <span className="productItemHash">{hashtxt1}</span>
+              <span className="productItemHash">{hashtxt2}</span>
+            </div>
+            <div className="productItemName">{title}</div>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Main;
