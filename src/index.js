77581321af48ca06720b13b69cb67bbeeebfd651
hjpark625 <zero950@gmail.com>
2022-06-21 09:49:06 +0900

    MODIFY: index.js내 scss경로 수정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
-import './styles/common.scss';
-import './styles/reset.scss';
+import './styles/Common.scss';
+import './styles/Reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
