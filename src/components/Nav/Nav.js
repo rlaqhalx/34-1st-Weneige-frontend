e695a8b9449b9e1fbc069afeb2f508e4074e0f6e
hjpark625 <zero950@gmail.com>
2022-06-21 21:08:44 +0900

    ADD: 초기 레이아웃 일부 완성

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,51 @@
 import React from 'react';
-import './nav.scss';
+import { Link } from 'react-router-dom';
+import './Nav.scss';
 
 const Nav = () => {
-  return <div>컴포넌트</div>;
+  return (
+    <nav className="navBar">
+      <div className="navLogo">
+        <Link to="#">
+          <img
+            src="https://www.laneige.com/kr/ko/assets/image/a/laneige-logo.svg"
+            alt="logo"
+          />
+        </Link>
+      </div>
+      <ul className="categories">
+        <li>
+          <span>브랜드</span>
+        </li>
+        <li>
+          <span>미츠 아트</span>
+        </li>
+        <li>
+          <span>베스트 & 신상품</span>
+        </li>
+        <li>
+          <span>스킨케어</span>
+        </li>
+        <li>
+          <span>메이크업</span>
+        </li>
+        <li>
+          <span>옴므</span>
+        </li>
+        <li>
+          <span>비스포크</span>
+        </li>
+        <li>
+          <span>이벤트</span>
+        </li>
+      </ul>
+      <div className="navIcons">
+        <i>아이콘1</i>
+        <i>아이콘2</i>
+        <i>아이콘3</i>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
