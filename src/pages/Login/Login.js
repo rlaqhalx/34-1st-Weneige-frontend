993972b79ea86abcbc5ae79faf1fc084270926dd
banhera <87149751+banhera@users.noreply.github.com>
2022-06-22 20:14:51 +0900

    ADD : 로그인 레이아웃 구현 1차 수정(리뷰반영)20220622

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,12 +3,12 @@ import './Login.scss';
 
 const Login = () => {
   return (
-    <div className="loginWrap">
+    <div className="login">
       <header className="loginHeader">
         <div className="loginHeaderBox">
           <div className="loginHeaderInner">
             <h1 className="loginPageTitle">로그인</h1>
-            <button type="button" className="loginClose" />
+            <button className="loginClose" />
           </div>
         </div>
       </header>
@@ -21,55 +21,42 @@ const Login = () => {
           </span>
         </div>
         <div className="secLogin">
-          <form id="login">
+          <form className="loginForm">
             <div className="loginInputForm">
-              <span className="inp" id="loginIdSpan">
+              <span className="inp">
                 <input
                   type="text"
-                  id="loginId"
+                  autoComplete="off"
                   className="inpText"
                   placeholder="아이디 입력"
                   title="아이디 입력"
                 />
-                <button
-                  type="button"
-                  className="btnDel"
-                  style={{ display: 'none' }}
-                >
+                <button type="button" className="btnDel">
                   <span className="blind">삭제</span>
                 </button>
               </span>
             </div>
             <div className="loginInputForm">
-              <span className="inp" id="passwordSpan">
+              <span className="inp">
                 <input
                   type="password"
-                  id="loginPassword"
                   autoComplete="off"
                   className="inpText"
                   placeholder="비밀번호 입력 (영문, 숫자, 특수문자 조합)"
                   title="비밀번호 입력"
                 />
-                <button
-                  type="button"
-                  className="btnDel"
-                  style={{ display: 'none' }}
-                >
+                <button type="button" className="btnDel">
                   <span className="blind">삭제</span>
                 </button>
               </span>
             </div>
-            <div id="loginNotiPanel">
-              <p id="loginNotiMsg" className="formTextError">
+            <div className="loginNotiPanel">
+              <p className="loginNotiMsg formTextError">
                 아이디 또는 비밀번호가 맞지 않습니다.
               </p>
             </div>
             <div className="loginOpt">
-              <button
-                type="button"
-                id="doLogin"
-                className="loginBtn loginBtnBlue"
-              >
+              <button type="button" className="loginBtn loginBtnBlue">
                 로그인
               </button>
             </div>
